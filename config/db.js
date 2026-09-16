443500ffc5476cfd8dd8e2a2dc95031217c0a77b
mytv9786 <mytv9786@gmail.com>
2026-09-16 18:47:09 +0530

    change

--- config/db.js
+++ config/db.js
@@ -14,12 +14,12 @@ dotenv.config();
 // });
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || process.env.DB_LOCAL_HOST,
-  user: process.env.DB_USER || process.env.DB_LOCAL_USER,
-  password: process.env.DB_PASSWORD || process.env.DB_LOCAL_PASSWORD,
-  database: process.env.DB_NAME || process.env.DB_LOCAL_NAME,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DBCAL_PASSWORD,
+  database: process.env.DB_NAME,
   //port: process.env.DB_PORT ? parseInt(process.env.DB_LOCAL_PORT) : 3306, // పోర్ట్ నంబర్ కోసం
-  port: process.env.PORT ? parseInt(process.env.PORT) : 3306,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
