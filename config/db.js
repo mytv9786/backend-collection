8595dfd1161a0cae62f9dec5e980827973b77151
saiteja1188 <loveui19@gmail.com>
2026-09-16 14:12:44 +0530

    fixed db port logic for render deployment

--- config/db.js
+++ config/db.js
@@ -14,11 +14,11 @@ dotenv.config();
 // });
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "s123",
-  database: process.env.DB_NAME || "collection",
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306, // పోర్ట్ నంబర్ కోసం
+  host: process.env.DB_HOST || process.env.DB_LOCAL_HOST,
+  user: process.env.DB_USER || process.env.DB_LOCAL_USER,
+  password: process.env.DB_PASSWORD || process.env.DB_LOCAL_PASSWORD,
+  database: process.env.DB_NAME || process.env.DB_LOCAL_NAME,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_LOCAL_PORT) : 3306, // పోర్ట్ నంబర్ కోసం
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
